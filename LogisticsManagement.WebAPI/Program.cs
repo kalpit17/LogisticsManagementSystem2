1def2bf94a0ea111e375edeb900654d51e1d45fc
kalpit <kalpitprajapati17@gmail.com>
2024-04-08 23:01:02 +0530

    Add project files.

---
+++ LogisticsManagement.WebAPI/Program.cs
@@ -0,0 +1,81 @@
+
+using LogisticsManagement.DataAccess.Models;
+using LogisticsManagement.DataAccess.Repository;
+using LogisticsManagement.DataAccess.Repository.IRepository;
+using LogisticsManagement.Service.Convertors;
+using LogisticsManagement.Service.DTOs;
+using LogisticsManagement.Service.Services;
+using LogisticsManagement.Service.Services.IServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace LogisticsManagement.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var configuration = builder.Configuration;
+
+            builder.Services.AddDbContext<LogisticsManagementContext>(option => option.UseSqlServer(configuration.GetConnectionString("localConnectionString")));
+           
+            builder.Services.AddAutoMapper(typeof(ApplicationMapper));
+
+
+            builder.Services.AddScoped<IAuthService,AuthService>();
+            builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
+            builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+            builder.Services.AddScoped<IAdminService, AdminService>();
+
+
+            //JWT Configuration
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata=false;
+                options.SaveToken=true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration.GetValue<string>("JWTSecret"))),
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["JWTIssuer"],
+
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWTAudience"],
+
+                    ClockSkew= TimeSpan.Zero
+                };
+            });
+
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
